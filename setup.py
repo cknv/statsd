f30ec9b71b99288c62042f50f5893c27ec337aae
Esben Sonne <esbensonne@gmail.com>
2016-05-31 09:12:53 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='charcoal',
-    version='0.1.0',
+    version='0.2.0',
 
     description='Opionionated high level StatsD client',
     long_description=long_description,
